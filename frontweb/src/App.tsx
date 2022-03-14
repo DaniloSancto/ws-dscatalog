ec884f0a57b70c9886bcb45e9fa8c75dcfc88f2a
Danilo Santos <danilo.santos0404@hotmail.com>
2022-03-14 05:50:07 -0300

    home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,12 @@
+import Home from 'pages/Home';
+
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from 'components/Navbar';
 
 const App = () => {
   return (
     <>
-      <Navbar/>
+      <Home/>
 
     </>
   );
